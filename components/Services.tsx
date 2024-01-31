9b4168c18b66a3b076ab0dcea1a0900f964a88fa
Dini <dini@Dinis-MacBook-Air.local>
2024-02-01 02:38:35 +0800

    modify services cards to show column for small devices and row for larger devices

--- components/Services.tsx
+++ components/Services.tsx
@@ -15,19 +15,19 @@ export default function Services() {
   ];
 
   return (
-    <section id="services" className="w-full bg-rose-100 py-20 border border-t-black">
+    <section id="services" className="w-full bg-rose-100 py-20 border border-t-black flex flex-col justify-center items-center">
       <div className="pb-20 flex justify-center">
         <RoundHeader text="SPECIALITIES" />
       </div>
 
-      <div className="flex justify-center px-5 md:px-10 max-w-[1500px]">{content && content.map(content => <Specialities key={content.header} header={content.header} description={content.description} />)}</div>
+      <div className="md:flex md:justify-between sm:justify-center px-5 md:px-10 max-w-[1500px]">{content && content.map(content => <Specialities key={content.header} header={content.header} description={content.description} />)}</div>
     </section>
   );
 }
 
 function Specialities({ header, description }: { header: string; description: string }) {
   return (
-    <div className="bg-white flex-1 max-w-[450px] mx-2 md:mx-10 lg:mx-auto p-5 md:p-10 rounded-xl border border-black">
+    <div className="bg-white max-w-[450px] mx-2 md:mx-10 lg:mx-auto border rounded-xl border-black md:w-1/2 lg:w-1/3 mb-10 shadow-md p-10">
       <h1 className="text-lg font-semibold">{header}</h1>
       <h3>{description}</h3>
     </div>
