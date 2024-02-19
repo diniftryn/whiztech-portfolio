41a8b5ee09906fd21091d3799ae65e65121404ab
Dini <dini@Dinis-MacBook-Air.local>
2024-02-20 00:44:02 +0800

    added animation for Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,16 +17,22 @@ export default function Header() {
 
   return (
     <header style={backgroundImageStyle} className="h-[450px] md:h-[650px] w-full text-center flex flex-col sm:gap-y-5">
-      <h2 className="text-3xl p-5">we turn your ideas into reality</h2>
+      <motion.h2 className="text-3xl p-5" initial={{ opacity: 0, y: -30 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 1.5 }}>
+        we turn your ideas into reality
+      </motion.h2>
 
-      <div className="md:flex md:flex-row md:gap-x-5 pt-5">
+      <motion.div className="md:flex md:flex-row md:gap-x-5 pt-5" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 1.5 }}>
         <Link href="#works">
-          <h1 className="border border-white rounded-3xl px-10 py-2 bg-white bg-opacity-20 hover:bg-opacity-50">view our work</h1>
+          <motion.h1 className="border border-white rounded-3xl px-10 py-2 bg-white bg-opacity-20 hover:bg-opacity-50" whileHover={{ scale: 1.1 }}>
+            view our work
+          </motion.h1>
         </Link>
         <Link href="#contact">
-          <h1 className="border border-black text-black rounded-3xl px-10 py-2 mt-5 md:mt-0 bg-black bg-opacity-20 hover:bg-opacity-50">chat with us</h1>
+          <motion.h1 className="border border-black text-black rounded-3xl px-10 py-2 mt-5 md:mt-0 bg-black bg-opacity-20 hover:bg-opacity-50" whileHover={{ scale: 1.1 }}>
+            chat with us
+          </motion.h1>
         </Link>
-      </div>
+      </motion.div>
     </header>
   );
 }
